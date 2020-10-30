49f86b97800392ce33a230169254fb63f5b4b4db
Sina Heidari <70712438+sinaheidari7@users.noreply.github.com>
2020-10-30 19:21:31 -0400

    Added files via upload
    Five problems

---
+++ myLearningKit.js
@@ -0,0 +1,285 @@
+/* 
+====
+Problem 15
+====
+*/
+function problem15() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm that receives a question and then outputs a random fortune.";
+
+  document
+    .getElementById("flowchart")
+    .setAttribute("src", "images/");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  
+  document.getElementById("js").setAttribute("src", "images/problem15.png");
+  document.getElementById("js").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+    
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+function zoomIn() {
+  document.getElementById("flowchart").style.width = "200%";
+}
+
+function zoomOut() {
+  document.getElementById("flowchart").style.width = "100%";
+}
+
+/* 
+====
+Problem 16
+=====
+ */
+function problem16() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm of rock, paper, scissors game. It should receive an input from user, generate a random choice and then determine the winner";
+
+
+  document
+    .getElementById("flowchart")
+    .setAttribute("src", "images/jujeh/jujehDesign.jpg");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem16.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 17
+=====
+ */
+function problem17() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and ouput the sum of its digits";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem17fc.jpeg");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem17.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 18
+=====
+ */
+function problem18() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and ouput its factorial";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem18fc.jpeg");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem18.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 19
+=====
+ */
+function problem19() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and output the fibonacci sequence up to the number inputted";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem19fc.jpeg");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem19.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 20
+=====
+ */
+function problem20() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and output whether if it is equal to its reverse or not";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem20fc.jpeg");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem20.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
