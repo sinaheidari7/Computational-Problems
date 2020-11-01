62a6f04c46c82f63b4f2f8d13c89086c883b2729
Sina Heidari <70712438+sinaheidari7@users.noreply.github.com>
2020-10-31 20:43:32 -0400

    Update myLearningKit.js

--- myLearningKit.js
+++ myLearningKit.js
@@ -275,6 +275,462 @@ function checkUncheck2() {
   }
 }
 
+/* 
+====
+Problem 21
+=====
+ */
+function problem21() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive three numbers of a quadratic equation and output the number of its roots";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem21fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem21.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 22
+=====
+ */
+function problem22() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive the width and height a rectange then output it's perimeter.";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem22fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem22.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 23
+=====
+ */
+function problem23() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive the semi-axes of an ellipsoid and then output its volume.";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem23fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem23.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 24
+=====
+ */
+function problem24() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a grade out of hundred and then output the numeral grade in letter grade form";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem24fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem24.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 25
+=====
+ */
+function problem25() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and output whether it is a positive or negative number and be disabled if the number is zero.";
+
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem25fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem25.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 26
+=====
+ */
+function problem26() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive multiple numbers and then output the count of positive and negative numbers.";
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem26fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem26.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 27
+=====
+ */
+function problem27() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and output whether if it is divisble by 6 or not";
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem27fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem27.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 28
+=====
+ */
+function problem28() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and return whether if it prime or not";
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem28fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem28.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 29
+=====
+ */
+function problem29() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to receive a number and return whether if it has a digit 7 or not";
+
+  document.getElementById("flowchart").setAttribute("src", "images/problem29fc.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem29.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+/* 
+====
+Problem 30
+=====
+ */
+function problem30() {
+  document.getElementById("problem").innerHTML =
+    "Devise an algorithm to flip a coin by getting a random number. If the number is below 0.5 set the value to tails if not set it heads. ";
+
+  document.getElementById("flowchart").setAttribute("src", "images.png");
+
+  document.getElementById("flowchart").style.display = "none";
+
+  document.getElementById("js").setAttribute("src", "images/problem30.png");
+  document.getElementById("js").style.display = "none";
+
+
+  document
+    .getElementById("another")
+    .setAttribute("src", "images/jujeh/jujeh2.jpg");
+  document.getElementById("another").style.display = "none";
+
+  document.getElementById("check1").checked = false;
+  document.getElementById("check2").checked = false;
+}
+
+
+
+function checkUncheck1() {
+  if (document.getElementById("check1").checked == true) {
+    document.getElementById("flowchart").style.display = "inline";
+  } else {
+    document.getElementById("flowchart").style.display = "none";
+  }
+}
+
+function checkUncheck2() {
+  if (document.getElementById("check2").checked == true) {
+    document.getElementById("js").style.display = "inline";
+  } else {
+    document.getElementById("js").style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
